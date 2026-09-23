f8d2eda9cd1721d7e96d63f17e38785dbd3908c1
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-09-23 21:47:31 +0530

    make a small application using streamlit and langchain chatbot

---
+++ 4.prompt/prompt_ui.py
@@ -0,0 +1,41 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_core.prompts import PromptTemplate,load_prompt
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-V4-Flash-0731",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+st.header('Research Tools')
+
+# user_input = st.text_input("Enter your Prompt here")
+
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+
+template = load_prompt("prompt/template.json")
+
+prompt = template.invoke({
+    'paper_input': paper_input,
+    'style_input': style_input,
+    'length_input': length_input
+})
+
+
+
+if st.button('Submit'):
+    # result = model.invoke(user_input)
+
+    result = model.invoke(prompt)
+
+    st.write(result.content)
